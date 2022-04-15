8465c0544bd37164e0735520e63ed3fc54254ae8
FoxGhost <63149175+FoxGhost@users.noreply.github.com>
2022-04-15 17:58:43 +0200

    video 5 end

--- 05RSA/VideoLectureCode/02_rsa_sing.c
+++ 05RSA/VideoLectureCode/02_rsa_sing.c
@@ -14,6 +14,7 @@ void handle_errors(){
 /*
     argv[1] = is the name of the file to sign
     argv[2] = is the name of the file where the private key is stored
+    openssl dgst -sha256 -signature signature.bin -verify public.pem 01_rsa_keygen.c
 */
 
 int main(int argc, char **argv){
@@ -42,15 +43,48 @@ int main(int argc, char **argv){
     */
     
     EVP_PKEY *private_key = PEM_read_PrivateKey(f_key, NULL, NULL, NULL);
+    fclose(f_key);
 
     EVP_MD_CTX *sign_ctx = EVP_MD_CTX_new();
 
     if(!EVP_DigestSignInit(sign_ctx, NULL, EVP_sha256(), NULL, private_key))
         handle_errors();
 
-    unsigned char buffer[MAXBUFFER]
+    unsigned char buffer[MAXBUFFER];
 
+    size_t n_read;
 
+    while ((n_read = fread(buffer, 1, MAXBUFFER, f_in)) > 0){
+        if(!EVP_DigestSignUpdate(sign_ctx, buffer, n_read))
+            handle_errors();
+    }
+    fclose(f_in);
+
+    unsigned char signature[EVP_PKEY_size(private_key)];
+
+    size_t signature_len;
+    size_t dgst_len;
+
+    if(!EVP_DigestSignFinal(sign_ctx, NULL, &dgst_len))
+        handle_errors();
+
+    if(!EVP_DigestSignFinal(sign_ctx, signature, &signature_len))
+        handle_errors();
+
+    EVP_MD_CTX_free(sign_ctx);
+
+    FILE *out;
+    if((out = fopen("signature.bin","w")) == NULL){
+        fprintf(stderr, "Problems creating the signature file\n");
+        abort();
+    }
+
+    if(fwrite(signature, 1, signature_len, out) < signature_len){
+        fprintf(stderr, "Couldn't dave data\n");
+        exit(1);
+    }
+    fclose(out);
+    printf("Signature written\n");
 
 
     CRYPTO_cleanup_all_ex_data();
