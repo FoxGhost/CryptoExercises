7014edde2ec87850a35688bb5da71dd5bca3426c
FoxGhost <63149175+FoxGhost@users.noreply.github.com>
2022-04-22 17:24:48 +0200

    Update rand1.c

--- OpenSLL/01RAND/VideoLectureCode/rand1.c
+++ OpenSLL/01RAND/VideoLectureCode/rand1.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
-
 #include <openssl/rand.h>
 #include <openssl/err.h>
 
 #define MAX 128
 
 void handle_errors(){
-    ERR_print_errors_fp(stderr);
-    abort();
+	ERR_print_errors_fp(stderr);
+	abort();
 }
 
-int main(){
-    unsigned char random_string[MAX];
+int main(int argc, char const *argv[])
+{
+	unsigned char random_string[MAX];
+	
+	if(RAND_load_file("/dev/random",64) != 64){
+		//ERR_print_errors_fp(stderr);
+		//fprintf(stderr, "Error with the initialization of the PRNG\n");
+		handle_errors();
+	}
 
-    if(RAND_load_file("/dev/random", 64) != 64)
-        handle_errors();
-    /*
-    RAND_bytes() and RAND_priv_bytes() return 1 on succes, 
-    -1 if not supported by the RAND method,
-    or 0 on another failure 
-    */
-   if (RAND_bytes(random_string, MAX) != 1)
-    handle_errors();
+//  RAND_bytes() and RAND_priv_bytes() return 1 on success, -1 if not supported by the current RAND method, or 0 on other failure
+	if(RAND_bytes(random_string, MAX) != 1){
+		//ERR_print_errors_fp(stderr);
+		//fprintf(stderr, "Error with the generation\n";
+		handle_errors();
+	}
 
-    printf("Sequence generated: ");
-    for (int i = 0; i < MAX; i++)
-        printf("%02x-", random_string[i]);
-    printf("\n");
+	printf("Sequence generated: ")
+	for (int i = 0; i < MAX; i++){
+		printf("%02x-", random_string[i]);
+	}
+	printf("\n");
 
-    return 0;    
+	return 0;
 }
