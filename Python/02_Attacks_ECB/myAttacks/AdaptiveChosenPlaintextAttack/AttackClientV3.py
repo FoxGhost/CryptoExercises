57e4b9f269ea342d85d408023f56638bc41ce9ba
FoxGhost <63149175+FoxGhost@users.noreply.github.com>
2022-05-03 19:00:51 +0200

    ECB ACPAv3 ok

--- Python/02_Attacks_ECB/myAttacks/AdaptiveChosenPlaintextAttack/AttackClientV3.py
+++ Python/02_Attacks_ECB/myAttacks/AdaptiveChosenPlaintextAttack/AttackClientV3.py
@@ -23,8 +23,9 @@ SECRET_LEN = 26
     4 block:  the secret is:1 -> this is the block that I want to compare
     5 block: AAAAAAAAAAAAAAAA -> padding to align the blocks to compare
     6 block: AAAAAAAAAA - and -> ...
-    7 block:  the secret is:* -> the block with the key char
-    8 block: ***************p
+    7 block:  the secret is:* -> the block with the secret char
+    8 block: ****************
+    9 block: *********ppppppp
 
     1 block: Here is the mess
     2 block: age:AAAAAAAAAAAA -> 
@@ -32,18 +33,27 @@ SECRET_LEN = 26
     4 block: the secret is:H1 -> the postfix reduce for the encreasing of the secret
     5 block: AAAAAAAAAAAAAAAA ->
     6 block: AAAAAAAAA - and  -> this pad is reducing for aligning the blocks to compare
-    7 block: the secret is:** -> the block with the key char
-    8 block: **************pp
-
-    do it until
+    7 block: the secret is:** -> the block with the secret char
+    8 block: ****************
+    9 block: ********pppppppp
 
     1 block: Here is the mess
-    2 block: age:AAAAAAAAAAAA -> 
-    3 block: AAAAAAAAAA - and -> there is no need to modify this pad
-    4 block: Here's my secre1 -> the postfix reduce for the encreasing of the secret
-    5 block: AAAAAAAAAAA - an ->
-    6 block: d the secret is: -> this pad is reducing for aligning the blocks to compare
-    7 block: **************** -> the block with the key char
+    2 block: age:AAAAAAAAAAAA  
+    3 block: AAAAAAAAAA - and 
+    4 block: Here's my very l 
+    5 block: AAAAAAAAAA - an 
+    6 block: d the secret is: 
+    7 block: ****************
+    8 block: *********ppppppp
+
+    1 block: Here is the mess
+    2 block: age:AAAAAAAAAAAA  
+    3 block: AAAAAAAAAAHere's 
+    4 block:  my very long se 
+    5 block: AAAAA - and the  
+    6 block: secret is:******
+    7 block: ****************
+    8 block: ****pppppppppppp
 """
 if __name__ == '__main__':
 
@@ -54,6 +64,7 @@ if __name__ == '__main__':
 
     prefix = "Here is the message:"
     postfix = " - and the secret is:"
+    postfix_len = len(postfix)
 
     prefix_pad_len = AES.block_size - (len(prefix) % AES.block_size)  # this is fix
     prefix_pad = "A" * prefix_pad_len
@@ -82,6 +93,10 @@ if __name__ == '__main__':
                 print("Found: " + char_to_guess)
                 secret += char_to_guess
                 postfix = postfix[1:]
+
+                if i >= postfix_len:
+                    postfix_pad = postfix_pad[1:]
+                    padding = padding[1:]
                 break
 
     print("The secret is: " + secret)
