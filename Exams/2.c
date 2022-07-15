c65f7c642141d1c64f566c378bb7f5847c796ab7
FoxGhost <63149175+FoxGhost@users.noreply.github.com>
2022-07-15 12:15:10 +0200

    ex 2 corrections
    bigger random numbers
    new c generation
    new final answer

--- Exams/2.c
+++ Exams/2.c
@@ -22,16 +22,16 @@ int int main(int argc, char const *argv[]){
 	BIGNUM *p = BN_new();
 	RAND_load_file("/dev/random", 64);
 	
-	BN_generate_prime_ex(p, 16, 0, NULL, NULL, NULL);
+	BN_generate_prime_ex(p, 64, 0, NULL, NULL, NULL);
 	
-	if (!BN_is_prime_ex(p,8,NULL,NULL)){
-		while(!BN_is_prime_ex(p,8,NULL,NULL)){
-			BN_generate_prime_ex(p, 16, 0, NULL, NULL, NULL);
+	if (!BN_is_prime_ex(p,32,NULL,NULL)){
+		while(!BN_is_prime_ex(p,32,NULL,NULL)){
+			BN_generate_prime_ex(p, 64, 0, NULL, NULL, NULL);
 		}
 	}
 
 	BIGNUM *G = BN_new();
-	BN_rand_ex(G, 16, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY, ctx);
+	BN_rand_ex(G, 64, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY, ctx);
 	BN_mod(G, G, p, ctx);
 
 
@@ -39,10 +39,17 @@ int int main(int argc, char const *argv[]){
 	send_to_sara(G);
 
 	BIGNUM *c = BN_new();
-	BN_rand_ex(c, 16, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY, ctx);
+	BN_rand_ex(c, 64, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY, ctx);
 	BN_mod(c, c, p, ctx);
+	/*
+			Another possible implementation without using the mod p
+			since the number must be 1 < c < p-1
+			to be sure that c isn't too small due to the module
+			BN_rand_ex(c, 63, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY, ctx);
+
+	*/
 	BIGNUM *gc = BN_new();
-	BN_mod_ex(gc, G, c, p, ctx);
+	BN_mod_exp(gc, G, c, p, ctx);
 
 	send_to_sara(gc)
 
@@ -70,7 +77,24 @@ int int main(int argc, char const *argv[]){
 }
 
 /*
-	Carl and Sara to securely share an AES256 key should use RSA to share it over an insecure channel
-	one of them generate the key, encrypt it with the public certificate of the other, 
+	Finally answer the following question: what CARL and SARA have to do if they want to generate an AES-256 key?
+*/
+
+/*
+	ANSWER 1 using the key generated 
+ 	One of the two has to generate the AES-256 key using PRNG
+ 	and then it will be encrypted using the key K obtained from DH as
+ 	AES*K mod p = AES KEY
+ 	The other side will decrypt it with AES_KEY*K^-1 mod p
+ 	And both will have the AES-256 key.
+ */
+/*
+	ANSWER 2 using RSA
+	Carl and Sara to securely share an AES-256 key should use RSA to share it over an insecure channel
+	one of them generate the key, encrypt it with the public key of the other, 
 	so the receiver can decrypt it with his private key
+
+	One of them generate the AES-256 with a PRNG
+	Encrypt it with the other's public key and send it
+	Once received the encrypted key the receiver can decrypt it with his private key
 */
